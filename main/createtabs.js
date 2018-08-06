e1b443570ebf3f3145a2df201bb51e3fb067ca8c
Savvas Pistikos <savpist@gmail.com>
2018-08-06 15:07:30 +0300

    Managed grouping

--- main/createtabs.js
+++ main/createtabs.js
@@ -5,6 +5,7 @@ var verbs = ["GET", "POST", "DELETE", "PUT"];
 var js;
 var importedJsonConfig;
 
+
 function importJson(liItem) {
     js = readJson(liItem.id + "swagger.json");
     var buttonElement = addButton(liItem.id);
@@ -17,7 +18,8 @@ function importJson(liItem) {
         s.setAttribute("type", "hidden");
         s.value = buttonElement.id.split("/")[1];
         di.appendChild(s);
-        var obj = js.paths;
+
+        let groupedPaths = groupByTags();
 
         let allCheckbox = document.createElement("input");
         allCheckbox.type = "checkbox";
@@ -38,70 +40,68 @@ function importJson(liItem) {
         version.setAttribute("id", "vers=" + buttonElement.id.split("/")[1]);
 
         ul.appendChild(allCheckbox);
+
         ul.appendChild(document.createTextNode("\t\t Url = "));
         ul.appendChild(url);
         ul.appendChild(document.createTextNode("\t\t version = "));
         ul.appendChild(version);
 
 
-        for (var path in obj) {
-            var li = document.createElement("li");
-            var checkbox = document.createElement('input');
-            checkbox.type = "checkbox";
-            var method = [];
-            if (obj[path]["get"] != null) {
-                method.push("GET");
+        for (let tag in groupedPaths) {
+            let tagli = document.createElement("li");
+            tagli.innerHTML = tag;
+
+            var pathsul = document.createElement("ul");
+
+            for (let path in groupedPaths[tag]) {
+                var pathli = document.createElement("li");
+                var internalpathul = document.createElement("ul");
+
+                for (let i in groupedPaths[tag][path].methods) {
+                    var ili = document.createElement("li");
+                    var icheckbox = document.createElement('input');
+                    icheckbox.type = "checkbox";
+                    icheckbox.setAttribute("onchange", "addToList(this);");
+                    icheckbox.setAttribute("class", buttonElement.id.split("/")[1]);
+                    icheckbox.setAttribute("id", groupedPaths[tag][path].endpoint + "," + groupedPaths[tag][path].methods[i]);
+                    let id = path + "," + groupedPaths[tag][path].methods[i];
+
+                    ili.appendChild(icheckbox);
+                    ili.appendChild(document.createTextNode(groupedPaths[tag][path].methods[i]));
+                    internalpathul.appendChild(ili);
+                }
+
+                var checkbox = document.createElement('input');
+                checkbox.type = "checkbox";
+                checkbox.setAttribute("onchange", "addToList(this);");
+                checkbox.setAttribute("class", buttonElement.id.split("/")[1]);
+                checkbox.setAttribute("id", "parent" + "=" + groupedPaths[tag][path].endpoint + "," + groupedPaths[tag][path].methods);
+
+                text = (js.basePath !== "/" && js.basePath != null) ? js.basePath + groupedPaths[tag][path].endpoint : groupedPaths[tag][path].endpoint;
+                pathli.appendChild(checkbox);
+                pathli.appendChild(document.createTextNode(text));
+                pathli.appendChild(document.createTextNode("\t\t Endpoint = "));
+
+                let endpointIn = document.createElement("input");
+                endpointIn.setAttribute("type", "text");
+                endpointIn.value = "";
+                //endpointIn.setAttribute("onfocus", "this.style.width = ((this.value.length + 4) * 7) + 'px';")
+                endpointIn.setAttribute("id", "end=" + groupedPaths[tag][path].endpoint);
+                pathli.appendChild(endpointIn);
+                pathli.appendChild(internalpathul);
+                pathsul.appendChild(pathli);
+                tagli.appendChild(pathsul);
+
+                ul.appendChild(tagli);
+                di.appendChild(ul);
+                di.setAttribute("class", "JsonContent");
+                di.setAttribute("id", "div=" + buttonElement.id.replace("bt=", ""));
+                di.style.display = "initial";
+                buttonElement.innerHTML = js.info.title;
+                document.body.appendChild(di);
             }
-            if (obj[path]["post"] != null) {
-                method.push("POST");
-            }
-            if (obj[path]["delete"] != null) {
-                method.push("DELETE");
-            }
-            if (obj[path]["put"] != null) {
-                method.push("PUT");
-            }
-            var iul = document.createElement("ul");
-
-            for (var i in method) {
-
-                var ili = document.createElement("li");
-                var icheckbox = document.createElement('input');
-                icheckbox.type = "checkbox";
-                icheckbox.setAttribute("onchange", "addToList(this);");
-                icheckbox.setAttribute("class", buttonElement.id.split("/")[1]);
-                icheckbox.setAttribute("id", path + "," + method[i]);
-                let id = path + "," + method[i];
-
-                ili.appendChild(icheckbox);
-                ili.appendChild(document.createTextNode(method[i]));
-                iul.appendChild(ili);
-            }
-
-            checkbox.setAttribute("onchange", "addToList(this);");
-            checkbox.setAttribute("class", buttonElement.id.split("/")[1]);
-            checkbox.setAttribute("id", "parent" + "=" + path + "," + method);
-
-            text = (js.basePath !== "/" && js.basePath != null) ? js.basePath + path : path;
-            li.appendChild(checkbox);
-            li.appendChild(document.createTextNode(text));
-            li.appendChild(document.createTextNode("\t\t Endpoint = "));
-            let endpointIn = document.createElement("input");
-            endpointIn.setAttribute("type", "text");
-            endpointIn.value = "";
-            //endpointIn.setAttribute("onfocus", "this.style.width = ((this.value.length + 4) * 7) + 'px';")
-            endpointIn.setAttribute("id", "end=" + path);
-            li.appendChild(endpointIn);
-            li.appendChild(iul);
-            ul.appendChild(li);
-
-            di.appendChild(ul);
-            di.setAttribute("class", "JsonContent");
-            di.setAttribute("id", "div=" + buttonElement.id.replace("bt=", ""));
-            di.style.display = "initial";
-            buttonElement.innerHTML = js.info.title;
-            document.body.appendChild(di);
         }
+
         hideOtherTabs(buttonElement.id);
     }
 }
@@ -207,4 +207,38 @@ function importConfig(impJson) {
             addToList(checkbox);
         }
     }
+}
+
+function groupByTags() {
+    let paths = js.paths;
+    let groupedPaths = {};
+    for (var p in paths) {
+        let pathObject = {endpoint: "", methods: []};
+        let tag;
+
+        if (paths[p]["get"] != null) {
+            pathObject.methods.push("GET");
+            tag = paths[p]["get"].tags;
+        }
+        if (paths[p]["post"] != null) {
+            pathObject.methods.push("POST");
+            tag = paths[p]["post"].tags;
+        }
+        if (paths[p]["delete"] != null) {
+            pathObject.methods.push("DELETE");
+            tag = paths[p]["delete"].tags;
+        }
+        if (paths[p]["put"] != null) {
+            pathObject.methods.push("PUT");
+            tag = paths[p]["put"].tags;
+        }
+        pathObject.endpoint = p;
+
+        if (groupedPaths[tag[0]] == null) {
+            groupedPaths[tag[0]] = [];
+        }
+        groupedPaths[tag[0]].push(pathObject);
+    }
+
+    return groupedPaths;
 }
