2bd2f08242e24d8b1b628f7aaa5f71f3d85f34e9
dreikk <savpist@gmail.com>
2018-08-07 22:56:09 +0300

    Moved the localstorage management to localstorage.js

--- main/managecheckboxes.js
+++ main/managecheckboxes.js
@@ -1,5 +1,3 @@
-var checkboxesStatus = {};
-
 function addToList(checkboxElem) {
     writetoLocalStorage(checkboxElem);
     if (listt[checkboxElem.className] == null) {
@@ -190,53 +188,6 @@ function getOnlyChildCheckboxesToBeremoved(checkboxElem) {
         return checkboxElem;
     }
 }
-
-function writetoLocalStorage(checkboxElem) {
-    checkboxesStatus[checkboxElem.id] = checkboxElem.checked;
-    localStorage.setItem("checkboxesStatus", JSON.stringify(checkboxesStatus));
-    localStorage.setItem("allGroupedByTags", JSON.stringify(allGroupedByTags));
-    localStorage.setItem("jsonList", JSON.stringify(jsonList));
-}
-
-
-$(document).ready(function () {
-    $(document).on('change', '#JsonContentDiv', function () {
-        localStorage.setItem("body", this.innerHTML);
-        localStorage.setItem("buttons", document.getElementById("buttons").innerHTML);
-    });
-});
-
-$(document).ready(function () {
-    $(document).on('click', '.remove', function () {
-        localStorage.setItem("body", document.getElementById("JsonContentDiv").innerHTML);
-        localStorage.setItem("buttons", document.getElementById("buttons").innerHTML);
-    });
-});
-
-//load
-$(document).ready(function () {
-    if (localStorage.getItem("body") != null && localStorage.getItem("buttons") != null && localStorage.getItem("checkboxesStatus") != null) {
-        document.getElementById("JsonContentDiv").innerHTML = localStorage.getItem("body");
-        document.getElementById("buttons").innerHTML = localStorage.getItem("buttons");
-        allGroupedByTags = JSON.parse(localStorage.getItem("allGroupedByTags"));
-        jsonList = JSON.parse(localStorage.getItem("jsonList"));
-        let checkboxses = JSON.parse(localStorage.getItem("checkboxesStatus"));
-
-        for (ch in checkboxses) {
-            let checkbox = document.getElementById(ch);
-            checkbox.checked = (checkboxses[ch] == true) ? true : false;
-            addToList(checkbox);
-        }
-
-    }
-});
-
-/*
-window.onbeforeunload = function() {
-    document.getElementById("JsonContentDiv").innerHTML = localStorage.getItem("body");
-    document.getElementById("buttons").innerHTML = localStorage.getItem("buttons");
-};
-*/
 
 
 
