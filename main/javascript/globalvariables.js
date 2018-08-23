1706f1f4768bec2676e601c0d3e1051e7368bc14
pistikos <pistikos@intralot.com>
2018-08-23 16:31:45 +0300

    added globalvariables.js

---
+++ main/javascript/globalvariables.js
@@ -0,0 +1,11 @@
+var listt = {};
+var jsonList = {};
+var tagList = {};
+var apiss = {apis: {}};
+var verbs = ["GET", "POST", "DELETE", "PUT"];
+var js;
+var importedJsonConfig;
+var allGroupedByTags = {};
+var outerdiv;
+var checkboxesStatus = {};
+var buttonIdWithout ;
